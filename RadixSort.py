5bc560f7bc12e7867303ff710315b59714657be2
Michael Malania <malaniamichael@gmail.com>
2020-03-23 04:42:15 +0400

    Radix Sort and Quick Sort

---
+++ RadixSort.py
@@ -0,0 +1,49 @@
+# Radix sort implementation
+import numpy as np
+import random
+import timeit
+
+start = timeit.default_timer()
+
+
+def countingSort(array, place):
+    size = len(array)
+    output = [0] * size
+    count = [0] * 10
+
+    for i in range(0, size):
+        index = array[i] // place
+        count[index % 10] += 1
+
+    for i in range(1, 10):
+        count[i] += count[i - 1]
+
+    i = size - 1
+    while i >= 0:
+        index = array[i] // place
+        output[count[index % 10] - 1] = array[i]
+        count[index % 10] -= 1
+        i -= 1
+
+    for i in range(0, size):
+        array[i] = output[i]
+
+
+def radixSort(array):
+    max_element = max(array)
+    place = 1
+    while max_element // place > 0:
+        countingSort(array, place)
+        place *= 10
+
+
+data = np.random.random_integers(20, size=(1000000))
+
+#data = [121, 432, 564, 23, 1, 45, 788]
+# data = random.sample(range(1, 1000001), 1000000) # In this case radix is slower but quick sort is way much faster
+radixSort(data)
+# print(data)
+
+stop = timeit.default_timer()
+
+print('Time: ', stop - start)
